725b60e4220534dc13a37fa8b0330e5c933fd316
rafazzaky <rafiazzakymc@gmail.com>
2024-03-12 19:05:04 +0700

    fix bug

--- src/server/handler.js
+++ src/server/handler.js
@@ -1,5 +1,6 @@
 const predictClassification = require('../services/inferenceService');
 const crypto = require('crypto');
+const storeData = require('../services/storeData')
  
 async function postPredictHandler(request, h) {
   const { image } = request.payload;
